c9dd5b71d2dc88f726a5649f5031ae9ec7e34fc6
Tayyab-tech88 <tayyabsaeed9811@gmail.com>
2025-05-26 10:20:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
+def data1():
+    return{"message":"hello"}
 
+@app.get("/api")
+def api():
+    return[
+        {"message":"hello my class"},
+        {"message":"hello world"}
+    ]
 
-
+@app.get("/ca")
+def hr():
+    return[
+        {"name":"Tayyab",
+         "age":19,
+         "class":"AI"
+         },
+         {"name":"Ali",
+         "age":23,
+         "class":"AI"
+         }
+    ]
